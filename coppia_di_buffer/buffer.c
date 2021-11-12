a9ad6f9bcd9e7bbc6d6df1fe1ddb697a9e09158a
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-11-12 19:36:27 +0100

    Refactorin

--- coppia_di_buffer/buffer.c
+++ coppia_di_buffer/buffer.c
@@ -6,44 +6,46 @@
 void produzione(int sem_id, buffer *buf1, buffer *buf2, int valore)
 {
 
+    buffer * buf;
+
     /* TBD: Sincronizzazione per inizio produzione */
+
+
+    /* TBD: Completare la ricerca del buffer LIBERO */
+
+    /* NOTA: Nel caso ci fossero stati più di un produttore,
+     * sarebbe necessario qui usare la mutua esclusione
+     * (MUTEX_P) per cerca il buffer LIBERO
+     */
 
     if (buf1->stato == /* TBD */)
     {
-        /* TBD: Porre il buffer 1 nello stato "in uso" */
-
         printf("[%d] In produzione su buf1...\n", getpid());
 
-        sleep(1);
+        buf = buf1;
 
-        buf1->valore = valore;
-
-        /* TBD: Porre il buffer 1 nello stato "occupato" */
-
-        printf("[%d] Prodotto il valore %d su buf1\n", getpid(), valore);
-    }
-    else
-    {
-
-        /* se il programma è arrivato a questo punto, vi
-         * deve essere almeno uno spazio disponibile, e
-         * quello spazio non è in "buf1", per cui "buf2"
-         * è necessariamente libero.
-         */
-
-        /* TBD: Porre il buffer 2 nello stato "in uso" */
+    } else {
 
         printf("[%d] In produzione su buf2...\n", getpid());
 
-        sleep(1);
-
-        buf2->valore = valore;
-
-        /* TBD: Porre il buffer 2 nello stato "occupato" */
-
-        printf("[%d] Prodotto il valore %d su buf2\n", getpid(), valore);
-
+        buf = buf2;
     }
+
+    buf->stato = /* TBD: Porre il buffer nello stato "in uso" */;
+
+    /* FINE RICERCA DEL BUFFER LIBERO */
+
+
+
+    sleep(1);
+
+    buf->valore = valore;
+
+    /* TBD: Porre il buffer nello stato "occupato" */
+
+    printf("[%d] Prodotto il valore %d\n", getpid(), valore);
+
+
 
     /* TBD: Sincronizzazione per fine produzione */
 }
@@ -51,49 +53,51 @@ void produzione(int sem_id, buffer *buf1, buffer *buf2, int valore)
 int consumazione(int sem_id, buffer *buf1, buffer *buf2)
 {
 
+    buffer * buf;
+
     int valore;
 
     /* TBD: Sincronizzazione per inizio consumazione */
 
 
+    /* TBD: Completare la ricerca del buffer OCCUPATO */
+
+    /* NOTA: Nel caso ci fossero stati più di un consumatore,
+     * sarebbe necessario qui usare la mutua esclusione
+     * (MUTEX_C) per cerca il buffer OCCUPATO
+     */
+
     if (buf1->stato == /* TBD */)
     {
-        /* TBD: Porre il buffer 1 nello stato "in uso" */
-
         printf("[%d] In consumazione su buf1...\n", getpid());
 
-        sleep(2);
+        buf = buf1;
 
-        valore = buf1->valore;
-
-        /* TBD: Porre il buffer 1 nello stato "libero" */
-
-        printf("[%d] Consumato il valore %d su buf1\n", getpid(), valore);
-
-    }
-    else
-    {
-        /* se il programma è arrivato a questo punto, vi
-         * deve essere almeno un messaggio disponibile, e
-         * quel messaggio non è in "buf1", per cui "buf2"
-         * è necessariamente occupato.
-         */
-
-        /* TBD: Porre il buffer 2 nello stato "in uso" */
+    } else {
 
         printf("[%d] In consumazione su buf2...\n", getpid());
 
-        sleep(2);
-
-        valore = buf2->valore;
-
-        /* TBD: Porre il buffer 2 nello stato "libero" */
-
-        printf("[%d] Consumato il valore %d su buf2\n", getpid(), valore);
-
+        buf = buf2;
     }
+
+    buf->stato = /* TBD: Porre il buffer nello stato "in uso" */;
+
+    /* FINE RICERCA DEL BUFFER OCCUPATO */
+
+
+
+    sleep(2);
+
+    valore = buf->valore;
+
+    /* TBD: Porre il buffer nello stato "libero" */
+
+    printf("[%d] Consumato il valore %d\n", getpid(), valore);
+
+
 
     /* TBD: Sincronizzazione per fine consumazione */
 
     return valore;
 }
+
